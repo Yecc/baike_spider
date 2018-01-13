4ef182930d35c3a1379b4a4a7d4bf405d239ed73
yangsu <yangsuuu@outlook.com>
2018-01-13 13:25:59 +0800

    建立框架

--- venv/spider_main.py
+++ venv/spider_main.py
@@ -7,6 +7,15 @@ class SpiderMain(object):
         self.parser = html_parser.HtmlParser()
         self.outputer = html_outputer.HtmlOutputer()
 
+    def craw(self,root_url):
+        self.urls.add_new_url(root_url)
+        while self.urls.has_new_url():
+            new_url = self.urls.get_new_url()
+            html_cont = self.downloader.download(new_url)
+            new_urls, new_data = self.parser.parse(new_url,html_cont)
+            self.urls.add_new_urls(new_urls)
+            self.outputer.collect_data(new_data)
+
 if __name__ == '__main__':
     root_url = ""
     obj_spider = SpiderMain()
